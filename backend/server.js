f7c8147c7e49d438b2f9eb5f9fc26d09e9d2e45b
ravindu629 <ravinduwijerathne629@gmail.com>
2024-05-24 01:19:14 +0530

    Setup connection to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import connectDB from "./config/db.js";
 import products from "./data/products.js";
 const port = process.env.PORT || 5000;
+
+//connect to mongo db
+connectDB();
 
 //initialize express
 const app = express();
