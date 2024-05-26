48b2b70618d1e519e80589f4dfddfd4c6a526a03
ravindu629 <ravinduwijerathne629@gmail.com>
2024-05-26 17:20:11 +0530

    Error middleware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 const port = process.env.PORT || 5000;
 
@@ -11,6 +12,9 @@ connectDB();
 
 //initialize express
 const app = express();
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.use("/api/products", productRoutes);
 
