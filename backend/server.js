033a8cd1144a02d046e05f3dd07ab1687b8d9d20
ravindu629 <ravinduwijerathne629@gmail.com>
2024-05-26 23:22:07 +0530

    Redux store, apiSlice & productSlice

--- backend/server.js
+++ backend/server.js
@@ -13,13 +13,9 @@ connectDB();
 //initialize express
 const app = express();
 
-app.use(notFound);
-app.use(errorHandler);
-
 app.use("/api/products", productRoutes);
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
