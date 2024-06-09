cf3391ab8020594570f8410783951676ef2084a8
ravindu629 <ravinduwijerathne629@gmail.com>
2024-06-10 03:38:52 +0530

    Logout user & clear JWT cookie

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,8 +4,9 @@ import {
   getProducts,
   getProductById,
 } from "../controllers/productController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
-router.route("/").get(getProducts);
+router.route("/").get(protect, admin, getProducts);
 router.route("/:id").get(getProductById);
 
 export default router;
