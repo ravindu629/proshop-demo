f301c6865affa9ed4e1ee4d25295e1ac88fb26a3
ravindu629 <ravinduwijerathne629@gmail.com>
2024-06-13 22:38:15 +0530

    auth & userApi slices

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,7 +6,7 @@ import {
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
-router.route("/").get(protect, admin, getProducts);
+router.route("/").get(getProducts);
 router.route("/:id").get(getProductById);
 
 export default router;
