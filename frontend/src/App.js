ad0d69ad38e7d1f1e4efd194e21a0740172ae94a
ravindu629 <ravinduwijerathne629@gmail.com>
2024-06-16 01:42:30 +0530

    Login screen & functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import { Container } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
+      <ToastContainer />
       <Header />
       <main className="py-3">
         <Container>
