6afbd550621561350c7780a22d80b645b67c97ef
ravindu629 <ravinduwijerathne629@gmail.com>
2024-06-23 00:52:47 +0530

    Admin product list & add exp to frontend user

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -70,6 +70,9 @@ const Header = () => {
               )}
               {userInfo && userInfo.isAdmin && (
                 <NavDropdown title="Admin" id="adminmenu">
+                  <LinkContainer to="/admin/productlist">
+                    <NavDropdown.Item>Products</NavDropdown.Item>
+                  </LinkContainer>
                   <LinkContainer to="/admin/orderlist">
                     <NavDropdown.Item>Orders</NavDropdown.Item>
                   </LinkContainer>
